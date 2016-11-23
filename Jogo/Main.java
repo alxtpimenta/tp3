cef7201efe90e13bd69d962c09c5b3d08dd65d15
anavelar <anavelar@users.noreply.github.com>
2016-11-23 21:57:24 -0200

    Update Main.java
    Coloquei o inicio pré-jogo

--- Jogo/Main.java
+++ Jogo/Main.java
@@ -5,7 +5,10 @@
  */
 package Jogo;
 
-import 
+import java.util.*;
+import Elementos.Jogador;
+import Propriedades.Definicoes;
+
 /**
  *
  * @author alexandrepimenta
@@ -23,6 +26,11 @@ public class Main
         
         int i;
         
+        String nome;
+        int cor;
+        int dados;
+        Random gerador;
+        
         /*
         Início do Jogo
         É aberta uma janela em que o usuário escolhe:
@@ -33,7 +41,7 @@ public class Main
          Com base na escolha, cria contador de tempo/passagens ou não cria.(Ou deixa a opção de trocar 
          de modo ligada durante o jogo (jogos que demoram muito) e inicia o contador de tempo/passadas de qualquer forma.)
          
-         -numero de Jogadores.
+         -numero de Jogadores. (entre MIN_JOGADORES E MAX_JOGADORES)
            Após ler retorna esse numero de jogadores para serem criados aqui.
         */
         
@@ -43,21 +51,43 @@ public class Main
         tempoMaxMinu = 60;
         //fim pedaço para testes
         
-        ArrayList<Jogador> obj = new ArrayList<Jogador>();
+        ArrayList<Jogador> jogadores = new ArrayList<Jogador>();
         
+        
+        gerador = new Random(); //gerador de números aleatórios
+        /* Código real que vamos usar
         for(i=0; i<numJogadores; i++)
         {
-            /*
-            Abre janela pedindo para inserir os dados relativos a cada jogador:
-            - nome
-            - cor escolhida
-            - lançar dados (retorna o resultado)
-            */   
+        	dados = gerador.nextInt(12)+1;
+        	
+            //Abre janela pedindo para inserir os dados relativos a cada jogador:
+            //- nome (variável nome)
+            //- cor escolhida (variável cor)
+            //- animação/imagem lançar os dados (exibe valor sorteado pela variavel dados aqui no inicio do bloco)
+            
+        	jogadores.add(new Jogador(nome, Definicoes.QUANTIA_INICIAL, cor, dados));
         }
+    	*/
+        
+        //Código temporário só enquanto a janela não fica pronta
+        dados = gerador.nextInt(12)+1;
+        jogadores.add(new Jogador("Alexandre", Definicoes.QUANTIA_INICIAL, 1, dados));
+        
+        dados = gerador.nextInt(12)+1;
+        jogadores.add(new Jogador("Ana Carolina", Definicoes.QUANTIA_INICIAL, 2, dados));
+        
+        dados = gerador.nextInt(12)+1;
+        jogadores.add(new Jogador("Ana Luiza", Definicoes.QUANTIA_INICIAL, 3, dados));
+        
+        dados = gerador.nextInt(12)+1;
+        jogadores.add(new Jogador("Pedro", Definicoes.QUANTIA_INICIAL, 4, dados));
+        //fim código temporário
+        
+        Collections.sort(jogadores); //jogadores ordenados no Array pela vez de jogar
 
         //Inicia a partida
         horaAtual = System.currentTimeMillis();
-        horaFinal  = horaAtual + (tempoMaxMinu x 60000);
+        horaFinal  = horaAtual + (tempoMaxMinu * 60000);
         
         
     }
