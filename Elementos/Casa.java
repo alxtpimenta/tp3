afc7470a84fe083d1b13a771aa50d4fea6b288db
alxtpimenta <alxtpimenta@icloud.com>
2016-11-26 20:01:23 -0200

    Deteccao de mouseover
    -Implementado metodo para calcular se o cursor esta dentro das paredes
    de cada casa
    -Implementado metodo publico para definir as coordenadas de cada casa
    (a partir do .txt ja gerado)

--- Elementos/Casa.java
+++ Elementos/Casa.java
@@ -11,24 +11,34 @@ package Elementos;
  */
 public abstract class Casa 
 {
+    //ATRIBUTOS
     protected String _nome;
     protected int _tipo;
-    protected int _coordX;
-    protected int _coordY;
-    //
-    
-    public int X()
-    {
-        return _coordX;
-    }
-    
-    public int Y()
-    {
-        return _coordY;
-    }
-    
+    protected int _x1;
+    protected int _x2;
+    protected int _y1;
+    protected int _y2;
+    //RETORNA O NOME DA CASA
     public String nome()
     {
         return _nome;
+    }
+    //DETECTA MOUSEOVER SOBRE A CASA
+    public boolean mouseOver(int x, int y)
+    {
+        //SE AS COORDENADAS DO MOUSE ESTIVEREM DENTRO DAS PAREDES DA CASA
+        //RETORNE TRUE, CASO CONTRARIO, RETORNE FALSE
+        if( (_x1 > x) && (_x2 < x) && (_y1 > y) && (_y1 < y) )
+            return true;
+        else
+            return false;
+    }
+    
+    public void setXY(int x1, int x2, int y1, int y2)
+    {
+        this._x1 = x1;
+        this._x2 = x2;
+        this._y1 = y1;
+        this._y2 = y2;
     }
 }
