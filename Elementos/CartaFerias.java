06860b8b7a2407368d4f5b8be458bc497c7bac76
alxtpimenta <alxtpimenta@icloud.com>
2016-12-20 21:14:23 -0200

    Caixas de dialogo

--- Elementos/CartaFerias.java
+++ Elementos/CartaFerias.java
@@ -8,8 +8,9 @@ public class CartaFerias extends Carta {
 	@Override
 	public void Efeito(Jogador jogador, ArrayList<Jogador> jogadores, int resultado_dados, ArrayList<Carta> cartas_ordem_tabuleiro,Deque<CartaSorteOuReves> cartas_sorte_ou_reves,ArrayList<CartaPropriedade> cartas_propriedades, ArrayList<CartaCompanhia> cartas_companhias)
 	{
-		//faz nada com o jogador
-		System.out.println("O jogador + " + jogador.getName() + " esta apenas visitando a cadeia! Que sorte! Sem efeitos nesta casa.");
+            //faz nada com o jogador
+            UserInterface.Dialogo.avisoGenerico("O jogador + " + jogador.getName() + " esta apenas visitando a cadeia! Que sorte! Sem efeitos nesta casa.");
+            //System.out.println("O jogador + " + jogador.getName() + " esta apenas visitando a cadeia! Que sorte! Sem efeitos nesta casa.");
 	}
 	
 }
